fe6a47d8ec12b0c25520416a777632ac69fb0679
sonoko furuya <ofu.sonoko@gmail.com>
2013-02-05 16:46:49 +0900

    [#43828209] travis CI 用の設定を追加

--- features/step_definitions/sample_website_steps.rb
+++ features/step_definitions/sample_website_steps.rb
@@ -1,8 +1,13 @@
 # encoding: utf-8
 
 もし /^"(.*?)"ページを表示する$/ do |page_name|
-  if page_name == "トップ"
+  case page_name
+  when "トップ"
     path = "/"
+  when "about"
+    path = "/about"
+  when "お問い合わせ先"
+    path = "/contacts"
   end
   visit path
 end
