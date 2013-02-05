b761eb5e7ad8d5c700eefb48d0ec94f7330ad421
sonoko furuya <ofu.sonoko@gmail.com>
2013-02-05 11:27:45 +0900

    Pages#homeをルートページとして追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SampleWebsite::Application.routes.draw do
+  get "pages/home"
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
